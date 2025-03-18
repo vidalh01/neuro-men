f3f4d4f974200c0080c90aed7c177d5e622c4b54
vidalh01 <vidalherrera01ec@gmail.com>
2025-03-17 22:56:25 -0400

    subiendo cambios

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,37 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/neuro-men/",
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "neuro-men",
+        short_name: "neuro men",
+        start_url: "/neuro-men/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#42b883",
+        icons: [
+          {
+            src: "/img/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/img/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
